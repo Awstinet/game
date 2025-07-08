a4e8f6d045e98220a21ae62a169102b73db6bfa1
Awstinet <149197678+Awstinet@users.noreply.github.com>
2025-07-08 09:38:53 +0200

    Create Portal.java

---
+++ src/Portal.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Portal {
+    
+    //Monde de départ, monde d'arrivée, coordonnées de spawn, 
+    //adapter le code des Maps pour qu'elles puisse recevoir un ou plusieurs portails en arguments
+
+}
